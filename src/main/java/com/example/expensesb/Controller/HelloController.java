8a636b57661d51b3ce3cad0cf0db81b9a5fe6375
Hamza Sbihi <shamza1235@gmail.com>
2025-12-25 18:42:38 +0100

    handling CORS erros globaly

--- src/main/java/com/example/expensesb/Controller/HelloController.java
+++ src/main/java/com/example/expensesb/Controller/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.expensesb.Controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/hello")
-@CrossOrigin(origins = "http://localhost:5173")
 public class HelloController {
 
     public HelloController() {
